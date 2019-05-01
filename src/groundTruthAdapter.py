adc3ed6e655d8cdab16953d36474800c7e76093e
fdabrowski <filip.dabrowski.pg@gmail.com>
2019-05-01 21:35:41 +0200

    finish groundTruthAdapter and add BoundingBox class

--- src/groundTruthAdapter.py
+++ src/groundTruthAdapter.py
@@ -1,19 +1,43 @@
 import os
 
+from src.BoundingBox import BoundingBox
+
+
 class GroundTruthAdapter():
     def __init__(self, projectName, fileName):
         self.fileName = fileName
         self.projectName = projectName
-        self.path = self.createPath()
         self.dirName = 'ground_truth_frames'
+        self.path = self.createPath()
 
     def openLabelsFile(self):
-        filePath = self.path +'/' + self.fileName
-        if(os.path.isfile(filePath))
-            self.file = open(filePath)
-            print(self.file)
+        filePath = self.path + '/' + self.fileName
+        if (os.path.isfile(filePath)):
+            return open(filePath, 'r+')
+
+    def getFileContent(self):
+        file = self.openLabelsFile()
+        boundingBoxesList = self.createBBList(file)
+        print(boundingBoxesList)
 
     def createPath(self):
-        path = self.dirName +'/' +self.projectName +'/labels'
-        if(os.path.isdir(path)):
+        path = '../' + self.dirName + '/' + self.projectName + '/labels'
+        if (os.path.isdir(path)):
             return path
+        else:
+            return ''
+
+    def getBBValues(self, file):
+        result = []
+        for line in file:
+            boxList = line.split()
+            result.append(boxList)
+        result.pop(0)
+        return result
+
+    def createBBList(self, file):
+        result = []
+        bbList = self.getBBValues(file)
+        for box in bbList:
+            result.append(BoundingBox(box[0], box[1], box[2], box[3], box[4]))
+        return result
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from src.groundTruthAdapter import GroundTruthAdapter
+
+gtAdapter = GroundTruthAdapter('traffic', 'frame1.txt')
+gtAdapter.getFileContent()
