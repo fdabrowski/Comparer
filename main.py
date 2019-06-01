c162a1b7334906621aa1118a5dd26e9c61eac027
fdabrowski <filip.dabrowski.pg@gmail.com>
2019-06-01 23:56:45 +0200

    add saving singular images

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import copy
 import sys
 from os import listdir
 from os.path import isfile, join
 import cv2
+import os
 import natsort
 from src.utils.FinalStatistics import avarageStatistics, saveFinalStatistics
 from src.IoUProvider import IoUProvider
@@ -16,10 +18,12 @@ from src.utils.boxDrawer import drawPredictedObjects, showConfidence
 TRAFFIC = 'traffic'
 NIGHT_STREET = 'night_street'
 DARK_TRAFFIC = 'dark_traffic'
-PROJECT_NAME = DARK_TRAFFIC
+LIGHT_TRAFFIC = 'light_traffic'
+BLUR_TRAFFIC = 'blur_traffic'
+PROJECT_NAME = TRAFFIC
 
-GT_FRAMES = '../ground_truth_frames/' +PROJECT_NAME +'/frames'
-GT_BOXES = '../ground_truth_frames/' +PROJECT_NAME +'/boxes'
+GT_FRAMES = '../ground_truth_frames/traffic/frames'
+GT_BOXES = '../ground_truth_frames/traffic/boxes'
 MASK_RCNN_BOXES = '../mask_RCNN/' +PROJECT_NAME +'/boxes'
 SSD_BOXES = '../ssd/' +PROJECT_NAME +'/boxes'
 YOLO_BOXES = '../yolo/' +PROJECT_NAME +'/boxes'
@@ -50,10 +54,14 @@ def run() -> None:
         fileName = sortedGtImages[index]
         imgcv = cv2.imread(GT_FRAMES + '/' + fileName)
 
+        imgcvYolo = copy.copy(imgcv)
+        imgcvRcnn = copy.copy(imgcv)
+        imgcvSSD = copy.copy(imgcv)
+
         gtBoundingBoxes = processGroundTruth(index, imgcv)
-        yoloBoundingBoxes, yoloTime = processYolo(index, imgcv)
-        rcnnBoundingBoxes, rcnnTime = processMaskRcnn(index, imgcv)
-        ssdBoundingBoxes, ssdTime = processSSD(index, imgcv)
+        yoloBoundingBoxes, yoloTime = processYolo(index, imgcv, imgcvYolo)
+        rcnnBoundingBoxes, rcnnTime = processMaskRcnn(index, imgcv, imgcvRcnn)
+        ssdBoundingBoxes, ssdTime = processSSD(index, imgcv, imgcvSSD)
 
         yoloPairs = createPairs(gtBoundingBoxes, yoloBoundingBoxes)
         maskRCNNPairs = createPairs(gtBoundingBoxes, rcnnBoundingBoxes)
@@ -83,26 +91,38 @@ def run() -> None:
 
     saveFinalResults(yoloAllStatistics, maskRcnnAllStatistics, ssdAllStatistics)
 
-def processYolo(index, imgcv):
+def processYolo(index, imgcv, imgCopy):
     yoloReader = JsonReader(PROJECT_NAME, sortedYoloFileList[index], 'yolo')
     yoloBoundingBoxes = yoloReader.getBoundingBoxes()
     time = yoloReader.getTime()
     # yoloBoundingBoxes = list(filter(lambda x: x.objectClass == 'person', yoloBoundingBoxes))
     drawPredictedObjects(yoloBoundingBoxes, imgcv, BoxColors.CAR_COLOR, 2)
+    drawPredictedObjects(yoloBoundingBoxes, imgCopy, BoxColors.CAR_COLOR, 2)
     showConfidence(yoloBoundingBoxes, imgcv, BoxColors.CAR_COLOR)
+    showConfidence(yoloBoundingBoxes, imgCopy, BoxColors.CAR_COLOR)
+    saveSingularImage(imgCopy, index, 'yolo')
     return yoloBoundingBoxes, time
 
-def processSSD(index, imgcv):
+def processSSD(index, imgcv, imgCopy):
     ssdReader = JsonReader(PROJECT_NAME, sortedSSDFileList[index], 'ssd')
     ssdBoundingBox = ssdReader.getBoundingBoxes()
     time = ssdReader.getTime()
     # ssdBoundingBox = list(filter(lambda x: x.objectClass == 'person', ssdBoundingBox))
     drawPredictedObjects(ssdBoundingBox, imgcv, BoxColors.SSD_COLOR, 2)
+    drawPredictedObjects(ssdBoundingBox, imgCopy, BoxColors.SSD_COLOR, 2)
     showConfidence(ssdBoundingBox, imgcv, BoxColors.SSD_COLOR)
+    showConfidence(ssdBoundingBox, imgCopy, BoxColors.SSD_COLOR)
+    saveSingularImage(imgCopy, index, 'ssd')
     return ssdBoundingBox, time
 
 def saveImage(imgcv, index):
     cv2.imwrite(OUT + '/' + str(index) + '_out.jpg', imgcv)
+
+def saveSingularImage(imgcv, index, algName):
+    path = OUT + '/' + algName + '/frame/'
+    if not os.path.exists(path):
+        os.makedirs(path)
+    cv2.imwrite(path + str(index) + '_out.jpg', imgcv)
 
 def processGroundTruth(index, imgcv):
     gtReader = GroundTruthReader(PROJECT_NAME, sortedGtFileList[index])
@@ -110,13 +130,16 @@ def processGroundTruth(index, imgcv):
     drawPredictedObjects(gtBoundingBoxes, imgcv, BoxColors.GT_COLOR, 2)
     return gtBoundingBoxes
 
-def processMaskRcnn(index, imgcv):
+def processMaskRcnn(index, imgcv, imgCopy):
     rcnnReader = JsonReader(PROJECT_NAME, sortedMaskRCNNFileList[index], 'mask_RCNN')
     rcnnBoundingBoxes = rcnnReader.getBoundingBoxes()
     time = rcnnReader.getTime()
     # rcnnBoundingBoxes = list(filter(lambda x: x.objectClass == 'person', rcnnBoundingBoxes))
     drawPredictedObjects(rcnnBoundingBoxes, imgcv, BoxColors.RCNN_COLOR, 2)
+    drawPredictedObjects(rcnnBoundingBoxes, imgCopy, BoxColors.RCNN_COLOR, 2)
     showConfidence(rcnnBoundingBoxes, imgcv, BoxColors.RCNN_COLOR)
+    showConfidence(rcnnBoundingBoxes, imgCopy, BoxColors.RCNN_COLOR)
+    saveSingularImage(imgCopy, index, 'mask_RCNN')
     return rcnnBoundingBoxes, time
 
 def saveFinalResults(yoloAllStatistics, maskRcnnAllStatistics, ssdAllStatistics):
@@ -134,7 +157,6 @@ def saveSingleResult(dir, pairs, iouResult, fileName):
 def createPairs(gtBoundingBoxes, predictBoundingBoxes):
     yoloPairBox = PairBox()
     return yoloPairBox.findPredicted(gtBoundingBoxes, predictBoundingBoxes)
-
 
 run()
 
--- videoCreator.py
+++ videoCreator.py
@@ -1,15 +1,17 @@
+import numpy as np
+
 import cv2
 import os
 
 import natsort
 # image_folder = 'ground_truth_frames/night_street/boxes'
-image_folder = 'ground_truth_frames/dark_traffic/boxes'
+image_folder = 'ground_truth_frames/traffic/frames'
 # image_folder = 'out/night_street'
 VIDEO_DIR = '/Users/filipdabrowski/Documents/video/'
-video_name = VIDEO_DIR + 'dark_traffic.avi'
+video_name = VIDEO_DIR + 'blur_traffic.avi'
 
-alpha = 0.1 # Simple contrast control
-beta = 20 # Simple brightness control
+alpha = 1 # Simple contrast control
+beta = 100# Simple brightness control
 
 images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
 frame = cv2.imread(os.path.join(image_folder, images[0]))
@@ -19,8 +21,9 @@ video = cv2.VideoWriter(video_name, 0, 29.9, (width,height))
 sortedGtImages = natsort.natsorted(images)
 for image in sortedGtImages:
     img = cv2.imread(os.path.join(image_folder, image))
-    imgDark = cv2.convertScaleAbs(img, alpha=alpha, beta=beta)
-    video.write(imgDark)
+    # imgDark = cv2.convertScaleAbs(img, alpha=alpha, beta=beta)
+    blur = cv2.blur(img, (15, 15))
+    video.write(blur)
 
 cv2.destroyAllWindows()
 video.release()
