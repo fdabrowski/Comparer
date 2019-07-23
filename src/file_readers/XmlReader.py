df0f7211a086b112254c3e156f7a4aceb2d0b48e
fdabrowski <filip.dabrowski.pg@gmail.com>
2019-07-24 00:15:07 +0200

    fix bugs in XmlReader

--- src/file_readers/XmlReader.py
+++ src/file_readers/XmlReader.py
@@ -2,6 +2,7 @@ import os
 from typing import List
 from xml.dom import minidom
 from xml.dom.minicompat import NodeList
+from xml.dom.minidom import Element
 
 from src.model.BoundingBox import BoundingBox
 
@@ -15,11 +16,11 @@ class XmlReader:
     def __init__(self, fileName) -> None:
         self.file_name = fileName
         self.out_path = XmlReader.path + self.file_name + '/boxes'
+        self.__create_out_dir()
 
-    def __open_box_file(self):
-        filePath = XmlReader.path + self.file_name
-        if (os.path.isfile(filePath)):
-            return open(filePath, 'r+')
+    def __create_out_dir(self):
+        if not os.path.exists(self.out_path):
+            os.makedirs(self.out_path)
 
     def __open_xml_file(self):
         return minidom.parse(XmlReader.path + self.file_name + '/' + self.file_name + '.xml')
@@ -29,14 +30,15 @@ class XmlReader:
 
     def __create_bouding_box_from_tag(self, box, label):
         return BoundingBox(
-            box.getElementsByTagName('xtl'),
-            box.getElementsByTagName('ytl'),
-            box.getElementsByTagName('xbr'),
-            box.getElementsByTagName('ybr'),
+            int(float(box.attributes['xtl'].value)),
+            int(float(box.attributes['ytl'].value)),
+            int(float(box.attributes['xbr'].value)),
+            int(float(box.attributes['ybr'].value)),
             label
         )
 
     def __save_txt(self, frame_file, bounding_box_list: List[BoundingBox]):
+        frame_file.write(str(len(bounding_box_list)) + '\n')
         for bounding_box in bounding_box_list:
             gt_values = str(bounding_box.downright_x) + ' ' + str(bounding_box.downright_y) + ' ' + str(
                 bounding_box.topleft_x) + ' ' + str(bounding_box.topleft_y) + ' ' + bounding_box.object_class + '\n'
@@ -48,20 +50,20 @@ class XmlReader:
             frame_file = open(self.out_path + '/frame' + str(number) + '.txt', 'w+')
             bounding_boxes = []
             for object in track_objects:
-                label = object.getElementsByTagName(XmlReader.label_tag)
+                label = object.attributes[XmlReader.label_tag].value
                 boxes = self.__get_boxes(object)
                 for box in boxes:
-                    frame_number = box.getElementsByTagName(XmlReader.frame_tag)
+                    frame_number = self.__get_frame_number(box)
                     if number == frame_number:
                         bounding_boxes.append(self.__create_bouding_box_from_tag(box, label))
-            self.__save_txt(frame_file, boxes)
+            self.__save_txt(frame_file, bounding_boxes)
 
 
     def __get_frames_number(self, xml_file) -> int:
         all_boxes = self.__get_boxes(xml_file)
         frames_number = 0
         for box in all_boxes:
-            box_frame = int(box.attributes[XmlReader.frame_tag].value)
+            box_frame = self.__get_frame_number(box)
             if box_frame > frames_number:
                 frames_number = box_frame
         return frames_number
@@ -69,6 +71,9 @@ class XmlReader:
 
     def __get_boxes(self, xml_file):
         return xml_file.getElementsByTagName(XmlReader.box_tag)
+
+    def __get_frame_number(self, box: Element):
+        return int(box.attributes[XmlReader.frame_tag].value)
 
     def convert_xml_to_txt(self):
         xml_file = self.__open_xml_file()
